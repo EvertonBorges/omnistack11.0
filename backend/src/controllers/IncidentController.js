870a7255551d1485b278685fbe8aac140e2788c7
EvertonBorges <evertonsoaresborges@gmail.com>
2020-03-29 11:24:24 -0400

    Ambiente de teste e validação de dados backend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -37,12 +37,17 @@ module.exports = {
         const { id } = request.params;
         const ong_id = request.headers.authorization;
 
-        const incident = await connection('incidents').where('id', id).select('ong_id').first();
-
-        if (incident.ong_id != ong_id) {
-            return response.status(401).json({
-                error: 'Operation not permitted.'
-            })
+        try {
+            const incident = await connection('incidents').where('id', id).select('ong_id').first();
+            if (incident.ong_id !== ong_id) {
+                return response.status(401).json({
+                    error: 'Operation not permitted.'
+                })
+            }
+        } catch(error) {
+            return response.status(404).json({
+                error: 'Incident not found.'
+            });
         }
 
         await connection('incidents').where('id', id).delete();
